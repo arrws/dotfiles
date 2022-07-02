28c6e363f83a70752bbe8b4816684fe39089bd4c
arrws <andreea@ieval.ro>
2022-07-02 16:11:32 +0100

    -

--- .scripts/warn_bat.sh
+++ .scripts/warn_bat.sh
@@ -2,12 +2,12 @@
 export DISPLAY=:0.0
 MyBat=$(cat /sys/class/power_supply/BAT0/capacity)
 
-if [[ $MyBat -lt 30 ]]; then
-    osd_cat -f -*-helvetica-*-r-*-*-32-*-*-*-*-*-*-* -A center -p middle -c yellow -d 15 <<< "
+if [[ $MyBat -lt 15 ]]; then
+    osd_cat -f -*-helvetica-*-r-*-*-32-*-*-*-*-*-*-* -A center -p middle -c red -d 30 <<< "
     low power low power low power low power low power low power low power low power low power low power low power low power low power low power
     " >& /home/nan/.osd.log
-elif [[ $MyBat -lt 15 ]]; then
-    osd_cat -f -*-helvetica-*-r-*-*-32-*-*-*-*-*-*-* -A center -p middle -c red -d 30 <<< "
+elif [[ $MyBat -lt 30 ]]; then
+    osd_cat -f -*-helvetica-*-r-*-*-32-*-*-*-*-*-*-* -A center -p middle -c yellow -d 15 <<< "
     low power low power low power low power low power low power low power low power low power low power low power low power low power low power
     " >& /home/nan/.osd.log
 fi
